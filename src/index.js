7630e26b3270a15cc53c9a6ec9a02a1a18c0706c
ISAIAS <issuyep@yahoo.com>
2023-05-05 18:30:51 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const tasksList = document.querySelector('#tasks');
+
+function deleteTask(event) {
+   event.preventDefault();
+  const taskItem = event.target.parentElement;
+  tasksList.removeChild(taskItem);
+}
+
+
+const createTaskForm = document.querySelector('#create-task-form');
+createTaskForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+
+
+  
+  const taskDescription = document.querySelector('#new-task-description').value;
+
+  
+  const taskItem = document.createElement('li');
+  taskItem.innerText = taskDescription;
+
+  
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'x';
+  deleteButton.addEventListener('click', deleteTask);
+  
+  
+  taskItem.appendChild(deleteButton);
+
+  tasksList.appendChild(taskItem);
+})
